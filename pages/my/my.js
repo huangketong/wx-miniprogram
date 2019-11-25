50c510d5052315ef7d468667ee71a428364906d6
ketong <ketong.hkt@alibaba-inc.com>
2019-11-25 16:51:40 +0800

    commit

--- pages/my/my.js
+++ pages/my/my.js
@@ -77,8 +77,11 @@ Page({
     // });
 
     // 写法 2
+    wx.showLoading({
+      title: '正在请求接口...',
+    })
     wx.request({
-      url: 'http://30.43.89.206:3000', //仅为示例，并非真实的接口地址
+      url: 'http://30.43.88.228:3000/api/wx-test', //仅为示例，并非真实的接口地址
       data: {},
       header: {
         'content-type': 'application/json' // 默认值
@@ -90,6 +93,7 @@ Page({
         this.setData({
           apiInfo: res.data
         })
+        wx.hideLoading();
       }
     });
   },
